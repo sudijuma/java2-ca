a60417c19e4dd358a747ab894e1c073a71132ea4
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-10-10 17:42:46 +0200

    update

---
+++ java2/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                home: resolve(__dirname, './index.html'),
+                login: resolve(__dirname, './login.html'),
+            },
+        },
+    }
+});
