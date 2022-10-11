d50642f398b234a0666c3083be293dc17b05cdad
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-10-11 19:41:21 +0200

    buildng posts.html

---
+++ java2/js/utilitys/storage.mjs
@@ -0,0 +1,43 @@
+const tokenKey = "token";
+const userKey = "user";
+
+function saveToken(token) {
+    saveToStorage(tokenKey, token);
+}
+
+function getToken() {
+    const value = localStorage.getItem(tokenKey);
+    if (value) {
+        return JSON.parse(value);
+    } else {
+        return null
+    }
+}
+
+function getUserName() {
+    const user = getFromStorage(userKey);
+    if (userKey) {
+        return user.name
+    } else {
+        return null;
+    }
+}
+
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+function getFromStorage(key) {
+    const value = localStorage.getItem(key);
+    if (value) {
+        return JSON.parse(value);
+    } else {
+        return []
+    }
+}
+
+function clearStorage() {
+    localStorage.clear();
+}
+
+export { getToken, saveToken, saveUser, getUserName, clearStorage }
---
+++ java2/js/utilitys/validation.js
@@ -0,0 +1,20 @@
+function validateEmail(email) {
+    const regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(stud.noroff.no|noroff.no)$/;
+    return email.match(regEx ? true : false)
+}
+
+function validatePassword(password, confirmPassword) {
+    if (!password) {
+        return false;
+    }
+    if (!confirmPassword) {
+        return false;
+    }
+    if (password !== confirmPassword) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export { validateEmail, validatePassword }
