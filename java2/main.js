e031bc2c7b6c7160e0e22aaa8f5e25b544baedeb
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-10-12 23:57:30 +0200

    more cleaning

--- java2/main.js
+++ java2/main.js
@@ -1,6 +1,1 @@
 import './scss/style.scss';
-import './scss/login.scss';
-import './scss/posts.scss';
-/* import './scss/landingPage.scss'; */
-
-//TODO clean this up, @forward and @use in other folder instead
