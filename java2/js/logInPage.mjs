6297e602aa9a7d87eb294e10312c2acdc8f5c782
sudijuma <91537436+sudijuma@users.noreply.github.com>
2022-10-12 16:10:41 +0200

    log 9j form

--- java2/js/logInPage.mjs
+++ java2/js/logInPage.mjs
@@ -1,6 +1,6 @@
-import { USER_LOGIN_URL } from "./API_URL/apiUrl.mjs";
+/* import { USER_LOGIN_URL } from "./API_URL/apiUrl.mjs"; */
 import { validateEmail } from "./utilities/validation.js";
-import { saveUser, saveToken } from "./utilities/storage.mjs";
+/* import { saveUser, saveToken } from "./utilities/storage.mjs"; */
 
 const logInForm = document.querySelector("#login-form");
 
@@ -11,3 +11,35 @@ const emailError = document.querySelector("#emailError");
 
 const password = document.querySelector("#password");
 const passwordNotValid = document.querySelector("#passwordError");
+const errorMassage = document.querySelector("error-massage");
+
+if (logInForm) logInForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let isEmail = false;
+    if (email.value.trim().length > 0) {
+        emailError.classList.add("hidden");
+        isEmail = true;
+    } else {
+        emailError.classList.remove("hidden");
+    }
+
+    let validEmail = false;
+    if (email.value.trim().length && validateEmail(email.value) === true) {
+        emailError.classList.add("hidden");
+        validEmail = true;
+    } else if (email.value.trim().length && validateEmail(email.value) !== true) {
+        emailNotValid.classList.remove("hidden");
+    }
+
+    let isPassword = false;
+    if (password.value.trim().length >= 8) {
+        passwordNotValid.classList.add("hidden");
+        isPassword = true;
+    } else {
+        passwordNotValid.remove("hidden");
+    }
+
+    let formIsValid = isEmail && validEmail && isPassword;
+    console.log(formIsValid)
+})
